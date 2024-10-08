d8e9c080757241ef0353a7956838ff1e9abe6e5e
ElouanBarois <elouan.barois@efpedu.fr>
2024-10-08 16:57:52 +0200

    Feat: Réorganisation de la page Région, Amélioration scss

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -10,13 +10,10 @@ import { Region } from 'models/region.model';
 export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         const regions: Region[] = [
-            { id: 1, name: 'LEC', logourl: 'https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/b/b4/G2_Esportslogo_profile.png', teams: ["FNC","G2","MAD","KC"],competitions: ["Regular Season", "Playoffs"] },
-            { id: 2, name: 'LCS', logourl: '../../assets/Explo.png', teams: ["TL","FLY","100T"],competitions: ["Regular Season", "Playoffs"] },
-            { id: 3, name: 'LCK', logourl: '../../assets/Explo.png', teams: ["T1","HLE","DK","GENG"],competitions: ["Regular Season", "Playoffs","Demacia Cup"] },
+            { id: 1, name: 'LEC', logourl: 'https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/3/30/LEC_2023.png', teams: ["FNC","G2","MAD","KC"],competitions: ["Regular Season", "Playoffs"] },
+            { id: 2, name: 'LCS', logourl: 'https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/2/24/LCS_2024.png', teams: ["TL","FLY","100T"],competitions: ["Regular Season", "Playoffs"] },
+            { id: 3, name: 'LCK', logourl: 'https://static.wikia.nocookie.net/lolesports_gamepedia_en/images/f/ff/LCK_2018_logo.png', teams: ["T1","HLE","DK","GENG"],competitions: ["Regular Season", "Playoffs","Demacia Cup"] },
         ];
-
-
-
         return { regions };
     }
 }
