a8af16c7594a5853d65b4297f46b138c0b3c667e
ThomasPloix <thomas.ploix@gmail.com>
2024-10-16 14:18:57 +0200

    [Add] Ajout TournamentService.ts

---
+++ src/app/tournament-package/service/TournamentService.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CompetitionService } from "../../services/competition.service"
+import { Competition } from "../../models/competition.model"
+import { Match } from "../../models/match.model"
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TournamentService {
+  competition: Competition | undefined
+  matches: Match[] | undefined
+
+  constructor(private competitionService : CompetitionService) {}
+
+  generateTree() {
+  this.competitionService.findById(3).subscribe((data) => {
+    this.matches = this.competition?.matches;
+    this.competition = data;
+  });
+  }
+
+  clearTournament() {
+
+  }
+
+
+  // Autres méthodes qui gèrent les interactions entre TreeManager et TeamHolderService
+}
